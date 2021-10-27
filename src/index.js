3e525517c47e66a9619e8fd4395ed276f2ee8b13
shaunvan119 <shaunvan@live.com.au>
2021-10-27 19:40:35 +1100

    event handler

--- src/index.js
+++ src/index.js
@@ -7,18 +7,21 @@ import './index.css'
 const books = [
 
 {
+  id:1,
   img: "https://images-na.ssl-images-amazon.com/images/I/91YZYvWIhgL._AC_UL200_SR200,200_.jpg",
   title: "Apples Never Fall",
   author: "Liane Moriarty",
 },
 
 {
+  id:2,
   img: "https://images-na.ssl-images-amazon.com/images/I/51yJZkx5sfL._AC_UL200_SR200,200_.jpg",
   title: "Costa's World",
   author: "Costa Georgiadis",
 },
 
 {
+  id:3,
   img: "https://images-na.ssl-images-amazon.com/images/I/812ufIGbAmL._AC_UL200_SR200,200_.jpg",
   title: "The Storyteller: Tales of Life and Music",
   author: "Dave Grohl",
@@ -31,23 +34,27 @@ function BookList() {
   return ( 
   <section className="booklist">
   {books.map((book)=> {
-    
-    return (
-      <Book book={book}></Book>
-    );
+    return <Book key={book.id} {...book}></Book>;
   })}
   </section>
    );  
   }
 
-const Book = (props) => {
-  const { img, title, author } = props.book;
+const Book = ({img, title, author}) => {
   
+  // attribute and eventhandler
+  // onclick and onMouseOver
+  const clickHandler = () => {
+alert("Hello World")
+  };
   return (
   <article className="book">
     <img src= {img}alt=""/>
     <h1>{title}</h1>
     <h4>{author}</h4>
+    <button type ="button" onClick={clickHandler}>
+      example
+    </button>
    
 
   </article>
